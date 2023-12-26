789c2de2dc6ae11cdeb4da0501c9e955e52a57ad
ParkYoDo <scarletdemon@naver.com>
2023-12-26 23:28:40 +0900

    webpack boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
 const App = () => {
-  return (
-    <div>
-      <div>PARKYODO's Blog</div>
-      <img src="favicon.ico" alt="" />
-      <img src="assets/test.png" alt="" />
-      <div>{process.env.REACT_APP_ENV}</div>
-    </div>
-  );
+  return <div>webpack-boilerplate</div>;
 };
 
 export default App;
